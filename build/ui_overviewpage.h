0efeffb95628fdf2957c445c6dc09c4f9d179968
root <root@astra1602.dedicatedpanel.com>
2017-11-05 17:21:02 +0100

    first commit

---
+++ build/ui_overviewpage.h
@@ -0,0 +1,401 @@
+/********************************************************************************
+** Form generated from reading UI file 'overviewpage.ui'
+**
+** Created by: Qt User Interface Compiler version 4.8.7
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_OVERVIEWPAGE_H
+#define UI_OVERVIEWPAGE_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QFormLayout>
+#include <QtGui/QFrame>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QListView>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_OverviewPage
+{
+public:
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacer_3;
+    QVBoxLayout *verticalLayout_2;
+    QSpacerItem *verticalSpacer;
+    QVBoxLayout *verticalLayout_3;
+    QFrame *frame_2;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_4;
+    QLabel *labelTransactionsStatus;
+    QSpacerItem *horizontalSpacer;
+    QListView *listTransactions;
+    QFrame *frame;
+    QVBoxLayout *verticalLayout_4;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label_5;
+    QLabel *labelWalletStatus;
+    QSpacerItem *horizontalSpacer_2;
+    QFormLayout *formLayout_2;
+    QLabel *label;
+    QLabel *labelBalance;
+    QLabel *label_6;
+    QLabel *labelStake;
+    QLabel *label_3;
+    QLabel *labelUnconfirmed;
+    QLabel *labelImmatureText;
+    QLabel *labelImmature;
+    QLabel *labelTotalText;
+    QLabel *labelTotal;
+    QSpacerItem *verticalSpacer_2;
+
+    void setupUi(QWidget *OverviewPage)
+    {
+        if (OverviewPage->objectName().isEmpty())
+            OverviewPage->setObjectName(QString::fromUtf8("OverviewPage"));
+        OverviewPage->resize(573, 342);
+        horizontalLayout = new QHBoxLayout(OverviewPage);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalSpacer_3 = new QSpacerItem(120, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_3);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_2->addItem(verticalSpacer);
+
+
+        horizontalLayout->addLayout(verticalLayout_2);
+
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        frame_2 = new QFrame(OverviewPage);
+        frame_2->setObjectName(QString::fromUtf8("frame_2"));
+        frame_2->setFrameShape(QFrame::StyledPanel);
+        frame_2->setFrameShadow(QFrame::Raised);
+        verticalLayout = new QVBoxLayout(frame_2);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        label_4 = new QLabel(frame_2);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        QPalette palette;
+        QBrush brush(QColor(110, 35, 31, 255));
+        brush.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        palette.setBrush(QPalette::Active, QPalette::Text, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Text, brush);
+        QBrush brush1(QColor(120, 120, 120, 255));
+        brush1.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::Text, brush1);
+        label_4->setPalette(palette);
+        QFont font;
+        font.setPointSize(7);
+        label_4->setFont(font);
+
+        horizontalLayout_2->addWidget(label_4);
+
+        labelTransactionsStatus = new QLabel(frame_2);
+        labelTransactionsStatus->setObjectName(QString::fromUtf8("labelTransactionsStatus"));
+        labelTransactionsStatus->setStyleSheet(QString::fromUtf8("QLabel { color: red; }"));
+        labelTransactionsStatus->setText(QString::fromUtf8("(out of sync)"));
+        labelTransactionsStatus->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
+
+        horizontalLayout_2->addWidget(labelTransactionsStatus);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_2->addItem(horizontalSpacer);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+        listTransactions = new QListView(frame_2);
+        listTransactions->setObjectName(QString::fromUtf8("listTransactions"));
+        QPalette palette1;
+        QBrush brush2(QColor(0, 0, 0, 0));
+        brush2.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::Button, brush2);
+        QBrush brush3(QColor(0, 0, 0, 255));
+        brush3.setStyle(Qt::NoBrush);
+        palette1.setBrush(QPalette::Active, QPalette::Base, brush3);
+        palette1.setBrush(QPalette::Active, QPalette::Window, brush2);
+        QBrush brush4(QColor(0, 0, 0, 255));
+        brush4.setStyle(Qt::SolidPattern);
+        palette1.setBrush(QPalette::Active, QPalette::Shadow, brush4);
+        palette1.setBrush(QPalette::Active, QPalette::AlternateBase, brush4);
+        palette1.setBrush(QPalette::Inactive, QPalette::Button, brush2);
+        QBrush brush5(QColor(0, 0, 0, 255));
+        brush5.setStyle(Qt::NoBrush);
+        palette1.setBrush(QPalette::Inactive, QPalette::Base, brush5);
+        palette1.setBrush(QPalette::Inactive, QPalette::Window, brush2);
+        palette1.setBrush(QPalette::Inactive, QPalette::Shadow, brush4);
+        palette1.setBrush(QPalette::Inactive, QPalette::AlternateBase, brush4);
+        palette1.setBrush(QPalette::Disabled, QPalette::Button, brush2);
+        QBrush brush6(QColor(0, 0, 0, 255));
+        brush6.setStyle(Qt::NoBrush);
+        palette1.setBrush(QPalette::Disabled, QPalette::Base, brush6);
+        palette1.setBrush(QPalette::Disabled, QPalette::Window, brush2);
+        palette1.setBrush(QPalette::Disabled, QPalette::Shadow, brush4);
+        palette1.setBrush(QPalette::Disabled, QPalette::AlternateBase, brush4);
+        listTransactions->setPalette(palette1);
+        QFont font1;
+        font1.setPointSize(11);
+        font1.setBold(true);
+        font1.setWeight(75);
+        listTransactions->setFont(font1);
+        listTransactions->setStyleSheet(QString::fromUtf8("QListView { background: transparent; }"));
+        listTransactions->setFrameShape(QFrame::NoFrame);
+        listTransactions->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        listTransactions->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        listTransactions->setSelectionMode(QAbstractItemView::NoSelection);
+
+        verticalLayout->addWidget(listTransactions);
+
+        frame = new QFrame(frame_2);
+        frame->setObjectName(QString::fromUtf8("frame"));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+        verticalLayout_4 = new QVBoxLayout(frame);
+        verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        label_5 = new QLabel(frame);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        QPalette palette2;
+        palette2.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        palette2.setBrush(QPalette::Active, QPalette::Text, brush);
+        palette2.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette2.setBrush(QPalette::Inactive, QPalette::Text, brush);
+        palette2.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
+        palette2.setBrush(QPalette::Disabled, QPalette::Text, brush1);
+        label_5->setPalette(palette2);
+        QFont font2;
+        font2.setPointSize(7);
+        font2.setBold(false);
+        font2.setWeight(50);
+        label_5->setFont(font2);
+
+        horizontalLayout_4->addWidget(label_5);
+
+        labelWalletStatus = new QLabel(frame);
+        labelWalletStatus->setObjectName(QString::fromUtf8("labelWalletStatus"));
+        labelWalletStatus->setStyleSheet(QString::fromUtf8("QLabel { color: red; }"));
+        labelWalletStatus->setText(QString::fromUtf8("(out of sync)"));
+        labelWalletStatus->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
+
+        horizontalLayout_4->addWidget(labelWalletStatus);
+
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer_2);
+
+
+        verticalLayout_4->addLayout(horizontalLayout_4);
+
+        formLayout_2 = new QFormLayout();
+        formLayout_2->setObjectName(QString::fromUtf8("formLayout_2"));
+        formLayout_2->setFieldGrowthPolicy(QFormLayout::AllNonFixedFieldsGrow);
+        formLayout_2->setHorizontalSpacing(12);
+        formLayout_2->setVerticalSpacing(12);
+        label = new QLabel(frame);
+        label->setObjectName(QString::fromUtf8("label"));
+        QFont font3;
+        font3.setBold(true);
+        font3.setWeight(75);
+        label->setFont(font3);
+
+        formLayout_2->setWidget(0, QFormLayout::LabelRole, label);
+
+        labelBalance = new QLabel(frame);
+        labelBalance->setObjectName(QString::fromUtf8("labelBalance"));
+        QPalette palette3;
+        palette3.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        palette3.setBrush(QPalette::Active, QPalette::Text, brush);
+        palette3.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette3.setBrush(QPalette::Inactive, QPalette::Text, brush);
+        palette3.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
+        palette3.setBrush(QPalette::Disabled, QPalette::Text, brush1);
+        labelBalance->setPalette(palette3);
+        QFont font4;
+        font4.setPointSize(10);
+        font4.setBold(true);
+        font4.setWeight(75);
+        labelBalance->setFont(font4);
+        labelBalance->setCursor(QCursor(Qt::IBeamCursor));
+        labelBalance->setText(QString::fromUtf8("0 EDDIE"));
+        labelBalance->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        formLayout_2->setWidget(0, QFormLayout::FieldRole, labelBalance);
+
+        label_6 = new QLabel(frame);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setFont(font3);
+
+        formLayout_2->setWidget(1, QFormLayout::LabelRole, label_6);
+
+        labelStake = new QLabel(frame);
+        labelStake->setObjectName(QString::fromUtf8("labelStake"));
+        QPalette palette4;
+        palette4.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        palette4.setBrush(QPalette::Active, QPalette::Text, brush);
+        palette4.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette4.setBrush(QPalette::Inactive, QPalette::Text, brush);
+        palette4.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
+        palette4.setBrush(QPalette::Disabled, QPalette::Text, brush1);
+        labelStake->setPalette(palette4);
+        labelStake->setFont(font4);
+        labelStake->setCursor(QCursor(Qt::IBeamCursor));
+        labelStake->setText(QString::fromUtf8("0 EDDIE"));
+        labelStake->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        formLayout_2->setWidget(1, QFormLayout::FieldRole, labelStake);
+
+        label_3 = new QLabel(frame);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setFont(font3);
+
+        formLayout_2->setWidget(2, QFormLayout::LabelRole, label_3);
+
+        labelUnconfirmed = new QLabel(frame);
+        labelUnconfirmed->setObjectName(QString::fromUtf8("labelUnconfirmed"));
+        QPalette palette5;
+        palette5.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        palette5.setBrush(QPalette::Active, QPalette::Text, brush);
+        palette5.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette5.setBrush(QPalette::Inactive, QPalette::Text, brush);
+        palette5.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
+        palette5.setBrush(QPalette::Disabled, QPalette::Text, brush1);
+        labelUnconfirmed->setPalette(palette5);
+        labelUnconfirmed->setFont(font4);
+        labelUnconfirmed->setCursor(QCursor(Qt::IBeamCursor));
+        labelUnconfirmed->setText(QString::fromUtf8("0 EDDIE"));
+        labelUnconfirmed->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        formLayout_2->setWidget(2, QFormLayout::FieldRole, labelUnconfirmed);
+
+        labelImmatureText = new QLabel(frame);
+        labelImmatureText->setObjectName(QString::fromUtf8("labelImmatureText"));
+        labelImmatureText->setFont(font3);
+
+        formLayout_2->setWidget(3, QFormLayout::LabelRole, labelImmatureText);
+
+        labelImmature = new QLabel(frame);
+        labelImmature->setObjectName(QString::fromUtf8("labelImmature"));
+        QPalette palette6;
+        palette6.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        palette6.setBrush(QPalette::Active, QPalette::Text, brush);
+        palette6.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette6.setBrush(QPalette::Inactive, QPalette::Text, brush);
+        palette6.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
+        palette6.setBrush(QPalette::Disabled, QPalette::Text, brush1);
+        labelImmature->setPalette(palette6);
+        labelImmature->setFont(font4);
+        labelImmature->setText(QString::fromUtf8("0 EDDIE"));
+        labelImmature->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        formLayout_2->setWidget(3, QFormLayout::FieldRole, labelImmature);
+
+        labelTotalText = new QLabel(frame);
+        labelTotalText->setObjectName(QString::fromUtf8("labelTotalText"));
+        labelTotalText->setFont(font3);
+
+        formLayout_2->setWidget(4, QFormLayout::LabelRole, labelTotalText);
+
+        labelTotal = new QLabel(frame);
+        labelTotal->setObjectName(QString::fromUtf8("labelTotal"));
+        QPalette palette7;
+        palette7.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        palette7.setBrush(QPalette::Active, QPalette::Text, brush);
+        palette7.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette7.setBrush(QPalette::Inactive, QPalette::Text, brush);
+        palette7.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
+        palette7.setBrush(QPalette::Disabled, QPalette::Text, brush1);
+        labelTotal->setPalette(palette7);
+        labelTotal->setFont(font4);
+        labelTotal->setCursor(QCursor(Qt::IBeamCursor));
+        labelTotal->setText(QString::fromUtf8("0 BTC"));
+        labelTotal->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        formLayout_2->setWidget(4, QFormLayout::FieldRole, labelTotal);
+
+
+        verticalLayout_4->addLayout(formLayout_2);
+
+
+        verticalLayout->addWidget(frame);
+
+
+        verticalLayout_3->addWidget(frame_2);
+
+        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer_2);
+
+
+        horizontalLayout->addLayout(verticalLayout_3);
+
+        horizontalLayout->setStretch(1, 1);
+        horizontalLayout->setStretch(2, 1);
+
+        retranslateUi(OverviewPage);
+
+        QMetaObject::connectSlotsByName(OverviewPage);
+    } // setupUi
+
+    void retranslateUi(QWidget *OverviewPage)
+    {
+        OverviewPage->setWindowTitle(QApplication::translate("OverviewPage", "Form", 0, QApplication::UnicodeUTF8));
+        label_4->setText(QApplication::translate("OverviewPage", "<b>Recent transactions</b>", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        labelTransactionsStatus->setToolTip(QApplication::translate("OverviewPage", "The displayed information may be out of date. Your wallet automatically synchronizes with the EDDIE network after a connection is established, but this process has not completed yet.", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        label_5->setText(QApplication::translate("OverviewPage", "MyEDDIECOINS", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        labelWalletStatus->setToolTip(QApplication::translate("OverviewPage", "The displayed information may be out of date. Your wallet automatically synchronizes with the EDDIE network after a connection is established, but this process has not completed yet.", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        label->setText(QApplication::translate("OverviewPage", "Spendable:", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        labelBalance->setToolTip(QApplication::translate("OverviewPage", "Your current spendable balance", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        label_6->setText(QApplication::translate("OverviewPage", "Stake:", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        labelStake->setToolTip(QApplication::translate("OverviewPage", "Total of coins that was staked, and do not yet count toward the current balance", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        label_3->setText(QApplication::translate("OverviewPage", "Unconfirmed:", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        labelUnconfirmed->setToolTip(QApplication::translate("OverviewPage", "Total of transactions that have yet to be confirmed, and do not yet count toward the current balance", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        labelImmatureText->setText(QApplication::translate("OverviewPage", "Immature:", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        labelImmature->setToolTip(QApplication::translate("OverviewPage", "Mined balance that has not yet matured", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        labelTotalText->setText(QApplication::translate("OverviewPage", "Total:", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        labelTotal->setToolTip(QApplication::translate("OverviewPage", "Your current total balance", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class OverviewPage: public Ui_OverviewPage {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_OVERVIEWPAGE_H
---
+++ src/obj/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2017-09-27 01:07:14 +0800"
